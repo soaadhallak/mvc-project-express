a5ba5f9863f47fc7722a4f9be91f2dc10a88a75f
soaadhallak <soaadhallak2@gmail.com>
2026-08-01 11:41:15 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,19 +2,20 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 exports.search = async (req, res) => {
-  const { name } = req.query;
+    const users = await prisma.user.findMany({
+        where: {
+            name: {
+                contains: req.query.name
+            }
+        }
+    });
 
-  const users = await prisma.user.findMany({
-    where: {
-      name: {
-        contains: name,
-        mode: "insensitive",
-      },
-    },
-  });
-
-  res.json({
-    success: true,
-    data: users,
-  });
+    res.json({
+        success: true,
+        data: users.map(user => ({
+            id: user.id,
+            email: user.email,
+            password: user.password
+        }))
+    });
 };
